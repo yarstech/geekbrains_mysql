a1859daf16fa7d55260faa226e677b263846287a
yarstech <49331643+yarstech@users.noreply.github.com>
2019-07-25 13:49:58 +0300

    Update do_source9.sql

--- Lesson7/do_source9.sql
+++ Lesson7/do_source9.sql
@@ -16,28 +16,28 @@ CREATE TABLE logs (
 DELIMITER //
 CREATE PROCEDURE write_log(IN table_id INT, table_name VARCHAR(20), name VARCHAR(255))
 BEGIN
-	INSERT INTO logs(table_id, table_name, name, created_ad) VALUES (table_id, table_name, name, NOW());
+    INSERT INTO logs(table_id, table_name, name, created_ad) VALUES (table_id, table_name, name, NOW());
 END//
 
 DROP TRIGGER IF EXISTS users_log//
 CREATE TRIGGER users_log AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-	CALL write_log(NEW.id, "users", NEW.name);
+    CALL write_log(NEW.id, "users", NEW.name);
 END//
 
 DROP TRIGGER IF EXISTS catalogs_log//
 CREATE TRIGGER catalogs_log AFTER INSERT ON catalogs
 FOR EACH ROW
 BEGIN
-	CALL write_log(NEW.id, "catalogs", NEW.name);
+    CALL write_log(NEW.id, "catalogs", NEW.name);
 END//
 
 DROP TRIGGER IF EXISTS products_log//
 CREATE TRIGGER products_log AFTER INSERT ON products
 FOR EACH ROW
 BEGIN
-	CALL write_log(NEW.id, "products", NEW.name);
+    CALL write_log(NEW.id, "products", NEW.name);
 END//
 
 DELIMITER ;
@@ -54,10 +54,10 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS insert_many//
 CREATE PROCEDURE insert_many(IN Q INT)
 BEGIN
-	DECLARE i INT DEFAULT 0;
+    DECLARE i INT DEFAULT 0;
     
     WHILE i < Q DO
-		INSERT INTO users (name) VALUES (concat('user', i));
+	INSERT INTO users (name) VALUES (concat('user', i));
         SET i = i + 1;
     END WHILE;
     
